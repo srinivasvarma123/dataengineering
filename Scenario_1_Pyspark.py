12dc2b55d7a604ab4472996cfad043baf625a9f3
Srinivas Varma Chintalapati <147342869+srinivasvarma123@users.noreply.github.com>
2024-12-20 20:11:46 -0500

    Update Scenario_1_Pyspark.py

--- Scenario_1_Pyspark.py
+++ Scenario_1_Pyspark.py
@@ -1,18 +1,7 @@
-from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-import sys
-import os
+spark = SparkSession.builder.appName("pyspark").getOrCreate()
 
-python_path = sys.executable
-os.environ['PYSPARK_PYTHON'] = python_path
-
-os.environ['JAVA_HOME'] = r'C:\Users\kvrkr\.jdks\corretto-1.8.0_432'
-
-conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
-sc = SparkContext(conf=conf)
-
-spark = SparkSession.builder.getOrCreate()
-
+sc = spark.sparkContext
 
 from pyspark.sql.functions import *
 data = [
